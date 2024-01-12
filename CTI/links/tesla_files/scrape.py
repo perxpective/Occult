c13533075ad7076fd8de44298370e334d4a12af5
jjroccss <jay.jay.tan.jing.kai@gmail.com>
2024-01-12 15:16:37 +0800

    CTI

---
+++ CTI/links/tesla_files/scrape.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+links=open('tesla_link.txt','r').read()
+links=links.split()
+count=0
+leftover=open('leftover.txt','w')
+for x in links:
+    try:
+        myConnection = urlopen(x).read()
+        png_header=bytes.fromhex('25504446')
+        if png_header in myConnection:
+            file=open(f'tesla_{count}.pdf','wb')
+            file.write(myConnection)
+            file.close()
+        else:
+            file=open(f'tesla_{count}.html','wb')
+            file.write(myConnection)
+            file.close()
+        count+=1
+    except:
+        leftover.write(x)
+leftover.close()
