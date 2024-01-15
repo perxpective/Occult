be2f87e9795e20b4ed9e407c227208939336c43d
perxpective <ervinlee2801@gmail.com>
2024-01-15 23:39:44 +0800

    Update connect.py

--- server/connect.py
+++ server/connect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e28980ad2e845efcb971fb7c4221da905d440a66a6d1c478cb5c8f15cbdb7f2c
-size 797
+oid sha256:85edc0c08d0d490c71c5f0f67a16b31c04b4f67f24ee86d9817c6b3b8e8bfb14
+size 803
